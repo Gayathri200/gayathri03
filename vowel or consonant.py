a76e5561777e8832ab447e6d043e11ac2157c663
gayathri.m <53171194+Gayathri200@users.noreply.github.com>
2019-07-23 15:02:29 +0530

    Add files via upload

---
+++ vowel or consonant.py
@@ -0,0 +1,7 @@
+def main():
+    x=input()
+    if x=='a' or x=='e' or x=='i' or x=='o' or x=='u'or x=='A'or x=='E' or x=='I' or x=='O' or x=='U':
+        print("Vowel")
+    else:
+        print("Consonant")
+main()
